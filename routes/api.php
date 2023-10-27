2cd1bb22fe1e0ef5ff9b5402eea6c4f5354d5dc2
ttrejosg <tomtrejosgil@gmail.com>
2023-10-26 20:30:40 -0500

    Protected routes

--- routes/api.php
+++ routes/api.php
@@ -15,26 +15,42 @@ use App\Http\Controllers\api\v1;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 // Users Routes
-Route::apiResource('v1/users', v1\UserController::class);
+Route::post('v1/users', [v1\UserController::class, 'store']);
 
 // Computers Routes
 Route::apiResource('v1/computers', v1\ComputerController::class)
-    ->except(['store']);
-Route::post('v1/users/{user}/computers', [v1\ComputerController::class, 'store']);
+    ->only(['index', 'show']);
 
 // Observations Routes
-Route::apiResource('v1/users.observations', v1\ObservationController::class)
-    ->only(['index', 'store', 'show']);
-Route::apiResource('v1/observations', v1\ObservationController::class)
-    ->only(['update', 'destroy']);
 Route::get('v1/computers/{computer}/observations', [v1\ObservationController::class, 'indexComputerObservations']);
 Route::get('v1/computers/{computer}/observations/{observation}', [v1\ObservationController::class, 'showComputerObservation']);
 
-// Categories Routes
-Route::apiResource('v1/categories', v1\CategoryController::class)
-    ->except(['update', 'show']);
+// Login
+Route::post('/v1/login', [App\Http\Controllers\api\v1\AuthController::class, 'login'])->name('api.login');
+
+// Authenticated Routes
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    // Users Routes
+    Route::apiResource('v1/users', v1\UserController::class)
+        ->except(['store']);
+
+    // Computers Routes
+    Route::put('v1/computers/{computer}', [v1\ComputerController::class, 'update']);
+    Route::delete('v1/computers/{computer}', [v1\ComputerController::class, 'destroy']);
+    Route::post('v1/users/{user}/computers', [v1\ComputerController::class, 'store']);
+
+    // Observations Routes
+    Route::apiResource('v1/users.observations', v1\ObservationController::class)
+        ->only(['index', 'store', 'show']);
+    Route::apiResource('v1/observations', v1\ObservationController::class)
+        ->only(['update', 'destroy']);
+
+    // Categories Routes
+    Route::apiResource('v1/categories', v1\CategoryController::class)
+        ->except(['update', 'show']);
+
+    // Logout
+    Route::post('/v1/logout', [App\Http\Controllers\api\v1\AuthController::class, 'logout'])->name('api.logout');
+});
